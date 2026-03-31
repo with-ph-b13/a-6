2481945133bf5341101a23284ec25ff945e10d2f
Ebtisam Jubair <abirmridha2005@gmail.com>
2026-03-31 17:18:30 +0600

    feat(UI): Navbar ; Basic project init files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col bg-white text-slate-900 font-sans">
+
+      <Navbar />
+
+    </div>
+  );
+}
+
+export default App;
